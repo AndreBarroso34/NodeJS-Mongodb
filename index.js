65517c15407899f5c0ef2f8cf061566d774004fe
André Barroso <132902074+AndreBarroso34@users.noreply.github.com>
2024-06-04 20:08:23 +0100

    index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+var Express =require("express");
+var MongoCLient=require("mongodb").MongoClient;
+var cors=require("cors");
+const multer=require("multer");
+const { MongoClient } = require("mongodb");
+
+var app=Express();
+app.use(cors());
+
+// FALTA POR A PASSWORD QUE ESTA NO BLOCO DE NOTAS DO PC DA ESCOLA
+var CONNECTION_STRING="mongodb+srv://a14381:<password>@cluster0.uyqerpv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+var DATEBASENAME="todoappdb";
+var database;
+
+app.listen(5038,()=> {
+    MongoClient.connect(CONNECTION_STRING,(error,client)=>{
+        database=client.db(DATEBASENAME);
+        console.log("Mongo DB Connection Successful");
+    })
+
+})
