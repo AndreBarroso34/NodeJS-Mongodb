d4b795d4ab6cbea50cee84b84acf50c1d6425e11
André Barroso <132902074+AndreBarroso34@users.noreply.github.com>
2024-06-05 08:14:15 +0000

    nova tese

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 var Express =require("express");
-var MongocLient=require("mongodb").MongoClient;
+var Mongoclient=require("mongodb").MongoClient;
 var cors=require("cors");
 const multer=require("multer");
-const { MongoClient } = require("mongodb");
+//const { MongoClient } = require("mongodb");
 
 var app=Express();
 app.use(cors());
 
 // FALTA POR A PASSWORD QUE ESTA NO BLOCO DE NOTAS DO PC DA ESCOLA
-var CONNECTION_STRING="mongodb+srv://a14381:<password>@cluster0.uyqerpv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+var CONNECTION_STRING="mongodb+srv://a14381:UjPlUnACAUIuCgdo@cluster0.uyqerpv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-var DATEBASENAME="todoappdb";
+var DATEBASENAME="turma2p2024";
 var database;
 
 app.listen(5038,()=> {
-    MongoClient.connect(CONNECTION_STRING,(error,client)=>{
+    Mongoclient.connect(CONNECTION_STRING,(error,client)=>{
         database=client.db(DATEBASENAME);
         console.log("Mongo DB Connection Successful");
     })
